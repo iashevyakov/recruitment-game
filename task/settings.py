df8fdefbf9fbf9d19330f9a0218e9bb1c58107c2
iashevyakov <iashevyakov@mail.ru>
2020-12-05 19:35:58 +0300

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z9fhda*dkyt9_8f!03_o2epx))1a*j=cuiyv+ikvvgj&o4^x)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['shevyakov-task.herokuapp.com', '127.0.0.1']
 
